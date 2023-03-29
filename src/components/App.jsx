6b335d9faad9cd2af3b7e6d7fe4716cd65d94059
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-03-29 21:53:08 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+// import hotBg from '../assets/hot.jpg'
+import coldBg from '../assets/cold.jpg'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="app" style={{ backgroundImage: `url(${coldBg})` }}>
+      <div className="overlay">
+        <div className="container">
+          <div className="section section__inputs">
+            <input type="text" className="" name='city' placeholder='Enter city...' />
+            <button className="">°F</button>
+          </div>
+          <section className="secttion__temperature"></section>
+          <div className="icon">
+            <h3>London</h3>
+            <div src='' alt='weatherIcon' className="img"></div>
+            <h3>Cloudy</h3>
+          </div>
+          <div className="temperature">
+            <h1>34 °C</h1>
+          </div>
+          {/* //TODO - bottom description */}
+        </div>
+      </div>
     </div>
-  );
-};
+  )
+}
